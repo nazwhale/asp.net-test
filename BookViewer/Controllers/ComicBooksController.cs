b8927c9978f33487fa778e1f4da7dfe38fb09696
Naz <nazmalik08@gmail.com>
2017-06-28 16:13:15 +0100

    Add layout file to index

--- BookViewer/Controllers/ComicBooksController.cs
+++ BookViewer/Controllers/ComicBooksController.cs
@@ -10,7 +10,7 @@ namespace BookViewer.Controllers
     {
         public ActionResult Index()
         {
-			ViewBag.Title = "Welcome to the bookatorium";
+			ViewBag.Title = "Welcome to The Bookatorium";
 			ViewBag.SubTitle = "Perveyors of peculiar books";
 			ViewBag.Text = "Here you can find all sorts of books. Big ones, small ones, some as big as your head. I don't even know how many books live here, it'd take me years to go through them all and frankly I just don't have the time.";
 			ViewBag.Authors = new string[]
