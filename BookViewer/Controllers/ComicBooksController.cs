ce933be92064f9da85ec8e3266758fed0eafbd7a
Naz <nazmalik08@gmail.com>
2017-06-28 12:53:59 +0100

    Change view vars to viewbag properties

--- BookViewer/Controllers/ComicBooksController.cs
+++ BookViewer/Controllers/ComicBooksController.cs
@@ -10,6 +10,16 @@ namespace BookViewer.Controllers
     {
         public ActionResult Index()
         {
+			ViewBag.Title = "Welcome to the bookatorium";
+			ViewBag.SubTitle = "Perveyors of peculiar books";
+			ViewBag.Text = "Here you can find all sorts of books. Big ones, small ones, some as big as your head. I don't even know how many books live here, it'd take me years to go through them all and frankly I just don't have the time.";
+			ViewBag.Authors = new string[]
+			{
+        		"Italo Calvino",
+        		"Luigi Seraphini",
+        		"Georges Perec"
+			};
+
             return View ();
         }
     }
