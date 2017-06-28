01f66f0ff019390feeb22e53e040147838cb0988
Naz <nazmalik08@gmail.com>
2017-06-28 14:51:06 +0100

    Add design folders to project

---
+++ BookViewer/BookViewer (copy)/Controllers/ComicBooksController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookViewer.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Index()
+        {
+			ViewBag.Title = "Welcome to the bookatorium";
+			ViewBag.SubTitle = "Perveyors of peculiar books";
+			ViewBag.Text = "Here you can find all sorts of books. Big ones, small ones, some as big as your head. I don't even know how many books live here, it'd take me years to go through them all and frankly I just don't have the time.";
+			ViewBag.Authors = new string[]
+			{
+        		"Italo Calvino",
+        		"Luigi Seraphini",
+        		"Georges Perec"
+			};
+
+            return View ();
+        }
+    }
+}
